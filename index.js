f6e2fc5a07ad31cb10f10230e9b7108460c54296
shawn <shawnsmith0024@gmail.com>
2022-12-16 09:04:01 -0500

    commit tree ecc0da2e68954591093b6a549e35d4865177be6f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    record.find(result)
+        if (result = "W") {
+            return year
+        }
+        else {
+            return 'Undefined'
+        }
+}
+superbowlWin();
